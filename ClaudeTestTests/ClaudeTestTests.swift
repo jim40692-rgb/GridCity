64591cd69b8a7faf23868a9012af7f6c80662291
Jru <jim40692@gmail.com>
2026-02-28 14:40:11 -0500

    Initial Commit

---
+++ ClaudeTestTests/ClaudeTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudeTestTests.swift
+//  ClaudeTestTests
+//
+//  Created by Jim Ruiz on 2/28/26.
+//
+
+import Testing
+@testable import ClaudeTest
+
+struct ClaudeTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
